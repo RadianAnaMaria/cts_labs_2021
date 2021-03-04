4c10e1e5b72408d387bc98e709eab2897b83efd1
40728 <40728@DESKTOP-OPRQ6AT>
2021-03-04 21:06:42 +0200

    merge

---
+++ HelloWorld1/src/ro/ase/csie/cts/lab/classes/TestApp.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.lab.classes;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+
+}
