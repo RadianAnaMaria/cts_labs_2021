c9a7ac577ce40fdccafe2305d8857a7830062afb
40728 <40728@DESKTOP-OPRQ6AT>
2021-03-10 20:30:45 +0200

    Lab2

---
+++ lab2/src/ro/ase/csie/g1088/laborator/exceptii/ExceptieFonduriInsuficiente.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.g1088.laborator.exceptii;
+
+public class ExceptieFonduriInsuficiente extends Exception {
+	public ExceptieFonduriInsuficiente(String mesaj) {
+		super(mesaj);
+	}
+	
+	public ExceptieFonduriInsuficiente() {
+		
+	}
+}
